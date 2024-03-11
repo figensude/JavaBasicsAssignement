bc530e9754c4564cb63786423452846c114e8951
figensude <162998747+figensude@users.noreply.github.com>
2024-03-11 16:12:11 +0300

    figen sude sanlı hafta3

---
+++ Homework_week3/javaislemler.java
@@ -0,0 +1,15 @@
+
+public class javaislemler {
+public static void main(String[]args) {
+	int sayi1,sayi2,sonuc1,sonuc2,sonuc3,sonuc4;
+	sayi1 = 25;
+	sayi2 = 5;
+	sonuc1 = sayi1+sayi2;
+	sonuc2 = sayi1-sayi2;
+	sonuc3 = sayi1*sayi2;
+	sonuc4 = sayi1/sayi2;
+	
+	
+	
+ }
+}
